eb3fe91e6af6cf75735374247f62b50c723f81e1
Richard Leshik <richardleshik@gmail.com>
2024-03-08 10:30:52 +0300

    feat: update index.js routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const multer = require('multer')
-const {UserController} = require("../controllers")
+const {UserController, PostController} = require("../controllers")
 const {authenticateToken} = require('../middleware')
 const router = express.Router()
 
@@ -20,5 +20,10 @@ router.post('/login', UserController.login)
 router.get('/current', authenticateToken, UserController.current)
 router.get('/users/:id', authenticateToken, UserController.getUserById)
 router.put('/users/:id', authenticateToken, UserController.updateUser)
+
+router.post('/posts', authenticateToken, PostController.createPost)
+router.get('/posts', authenticateToken, PostController.getAllPosts)
+router.get('/posts/:id', authenticateToken, PostController.getPostById)
+router.delete('/posts/:id', authenticateToken, PostController.deletePost)
 
 module.exports = router
