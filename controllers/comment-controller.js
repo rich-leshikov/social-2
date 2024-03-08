44507670e1830c2976f94ce0836e957fef66d4bf
Richard Leshik <richardleshik@gmail.com>
2024-03-08 21:00:05 +0300

    feat: implement createComment function body

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -2,7 +2,27 @@ const {prisma} = require('../prisma/prisma-client')
 
 const CommentController = {
   createComment: async (req, res) => {
-    res.send('createComment')
+    const {postId, content} = req.body
+    const userId = req.user.userId
+
+    if (!postId || !content) {
+      return res.status(400).json({error: 'All fields required'})
+    }
+
+    try {
+      const comment = await prisma.comment.create({
+        data: {
+          postId,
+          userId,
+          content
+        }
+      })
+
+      res.json(comment)
+    } catch (error) {
+      console.error('Create comment error', error)
+      res.status(500).json({error: 'Internal server error'})
+    }
   },
   deleteComment: async (req, res) => {
     res.send('deleteComment')
