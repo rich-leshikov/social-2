bc360a9d3ef282065f8a46c038f8e4f95687c27d
Richard Leshik <richardleshik@gmail.com>
2024-03-09 13:33:04 +0300

    feat: implement likePost function body

--- controllers/like-controller.js
+++ controllers/like-controller.js
@@ -2,7 +2,31 @@ const {prisma} = require('../prisma/prisma-client')
 
 const LikeController = {
   likePost: async (req, res) => {
-    res.send('likePost')
+    const {postId} = req.body
+    const userId = req.user.userId
+
+    if (!postId) {
+      return res.status(400).json({error: 'All fields required'})
+    }
+
+    try {
+      const existingLike = await prisma.like.findFirst({
+        where: {postId, userId}
+      })
+
+      if (existingLike) {
+        return res.status(400).json({error: 'You have already liked this post'})
+      }
+
+      const like = await prisma.like.create({
+        data: {postId, userId}
+      })
+
+      res.json(like)
+    } catch (error) {
+      console.error('Like post error', error)
+      res.status(500).json({error: 'Internal server error'})
+    }
   },
   unlikePost: async (req, res) => {
     res.send('unlikePost')
