7d4b07491bf805ee34d9a3f997df63a6f49efbf4
Richard Leshik <richardleshik@gmail.com>
2024-03-11 22:25:58 +0300

    feat: add postApi

---
+++ client/src/app/services/postApi.ts
@@ -0,0 +1,45 @@
+import { api } from "./api"
+import type { Post } from "../types"
+
+export const postApi = api.injectEndpoints({
+  endpoints: builder => ({
+    createPost: builder.mutation<Post, { content: string }>({
+      query: postData => ({
+        url: "/posts",
+        method: "POST",
+        body: postData,
+      }),
+    }),
+    getAllPosts: builder.query<Post[], void>({
+      query: () => ({
+        url: "/posts",
+        method: "GET",
+      }),
+    }),
+    getPostById: builder.query<Post, string>({
+      query: id => ({
+        url: `/posts/${id}`,
+        method: "GET",
+      }),
+    }),
+    deletePost: builder.mutation<Post[], string>({
+      query: id => ({
+        url: `/posts/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+})
+
+export const {
+  useCreatePostMutation,
+  useGetAllPostsQuery,
+  useLazyGetAllPostsQuery,
+  useGetPostByIdQuery,
+  useLazyGetPostByIdQuery,
+  useDeletePostMutation,
+} = postApi
+
+export const {
+  endpoints: { createPost, getAllPosts, getPostById, deletePost },
+} = postApi
