bac388117f9414b7763cd12a4db44e5463655ce8
Richard Leshik <richardleshik@gmail.com>
2024-03-13 12:47:08 +0300

    feat: add router

--- client/src/common/routes/router.tsx
+++ client/src/common/routes/router.tsx
@@ -1,1 +1,12 @@
-export const router = {}
+import { createBrowserRouter } from "react-router-dom"
+
+export const router = createBrowserRouter([
+  {
+    path: "/auth",
+    element: <h1>Auth</h1>,
+  },
+  {
+    path: "/",
+    element: <h1>Home</h1>,
+  },
+])
