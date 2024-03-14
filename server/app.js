9bf45324a2630ed581471388fc84da434c5216aa
Richard Leshik <richardleshik@gmail.com>
2024-03-14 19:28:44 +0300

    feat: add cors usage

--- server/app.js
+++ server/app.js
@@ -4,14 +4,17 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const fs = require('fs')
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'jade')
 // share static files from folder 'uploads'
 app.use('/uploads', express.static('uploads'))
