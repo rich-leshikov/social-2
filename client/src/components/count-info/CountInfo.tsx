d385085babfb4ae35a41d64c2e6557e91f62a39a
Richard Leshik <richardleshik@gmail.com>
2024-03-17 23:52:49 +0300

    feat: add CountInfo component

---
+++ client/src/components/count-info/CountInfo.tsx
@@ -0,0 +1,15 @@
+import type { FC } from "react"
+
+type CountInfoProps = {
+  count: number
+  title: string
+}
+
+export const CountInfo: FC<CountInfoProps> = ({ count, title }) => {
+  return (
+    <div className={"flex flex-col items-center space-x-2 p-4"}>
+      <span className="text-4xl font-semibold">{count}</span>
+      <span>{title}</span>
+    </div>
+  )
+}
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./button"
 export * from "./card"
 export * from "./container"
+export * from "./count-info"
 export * from "./create-comment"
 export * from "./create-post"
 export * from "./error-message"
